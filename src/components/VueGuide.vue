e64a44b1982555d121c014c60c72643c8fbde7a4
NeXT___ <kingideayou@foxmail.com>
2016-10-21 16:48:16 +0800

    vue start

---
+++ src/components/VueGuide.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="vue_1">
+    <span>Message: {{ msg }}</span>
+    <br/><br/>
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+    <br><br>
+    <p v-if="seen">
+      Now you see me.
+    </p>
+    <br><br>
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <br><br>
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+    <br><br>
+    <p>{{ message }}</p>
+    <input v-model="message">
+    <br><br>
+    <ul>
+      <todo-item></todo-item>
+    </ul>
+    <br><br>
+    <ol>
+      <todo v-for="todo in todos" v-bind:todo="todo"></todo>
+    </ol>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'vue_1',
+  data () {
+      return {
+        msg: 'Hello World.',
+        message: 'You loaded this page on ' + new Date(),
+        seen: true,
+        todos: [
+          { text: 'Learn JS'},
+          { text: 'Learn Vue'},
+          { text: 'Build something awesome'}
+        ]
+      }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
+  components: {
+  }
+}
+
+</script>
---
+++ src/components/componentA.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{msgFromFather}}</h1>
+    <a v-bind:href="url"></a>
+    <button @click="onClickMe">Click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'This is ComponentA'
+    }
+  },
+  props: ['msgFromFather'],
+  methods: {
+    onClickMe: function () {
+      console.log(this.msgFromFather)
+      this.$emit('child-tell-me-something', this.msg)
+      // this.$dispatch('child-tell-me-something', this.msg)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
