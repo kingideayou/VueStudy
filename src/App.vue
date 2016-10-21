72e706395a13e5e7db2f53e5e85fd2ba5d605803
NeXT <youwenbo@xitu.io>
2016-10-21 16:48:16 +0800

    vue start

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="app">
+
+    <VueGuide></VueGuide>
+
+    <!--
+    <h1>{{title}}</h1>
+    <input v-model='newItem' @keyup.enter="addNewItem">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}"
+            @click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
+    -->
+
+    <!--
+    <ComponentA msgFromFather='father teach you'
+        v-on:child-tell-me-something='listenToMyBoy'></ComponentA>
+    <p>子视图对我说：{{ childWords}}</p>
+    -->
+    <!--
+    <img src="./assets/logo.png">
+    <hello></hello>
+    -->
+  </div>
+</template>
+<script>
+
+import Hello from './components/Hello'
+import Store from './store'
+import ComponentA from './components/componentA'
+import VueGuide from './components/VueGuide'
+
+export default {
+  data: function (){
+    return {
+      title: 'This is a todo list',
+      childWords: '',
+      //items: Store.fetch(),
+      items: [
+        {
+          label: 'codding',
+          isFinished: false
+        },
+        {
+          label: 'walking',
+          isFinished: true
+        }
+      ],
+      liClass: 'thisIsLiClass',
+      newItem: ''
+    }
+  },
+  name: 'app',
+  components: {
+    ComponentA,
+    Hello,
+    VueGuide
+  },
+  watch: {
+    items: {
+      handler: function (items) {
+        Store.save(items)
+      },
+      deep: true //单个 key 更改，也会更新列表
+    }
+  },
+  /*
+  events: {
+    'child-tell-me-something': function (msg) {
+      this.childWords = msg;
+    }
+  },
+  */
+  methods: {
+    toggleFinish: function (item) {
+      item.isFinished = !item.isFinished
+    },
+    addNewItem: function() {
+      console.log(this.newItem)
+      this.items.push(
+        {
+          label: this.newItem,
+          isFinished: false
+        }
+      )
+      this.newItem = ''
+    },
+    listenToMyBoy: function (msg) {
+      this.childWords = 'child ...'
+    }
+  }
+  /*
+  name: 'app',
+  components: {
+    Hello
+  }
+  */
+}
+</script>
+
+<style>
+.finished {
+  text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
