78b70421b034347fde42ee9c003b42d297db4285
NeXT___ <kingideayou@foxmail.com>
2016-10-24 12:46:06 +0800

    vue resource

--- src/App.vue
+++ src/App.vue
@@ -154,5 +154,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0, auto;
 }
 </style>
--- src/components/VueResourceDemo.vue
+++ src/components/VueResourceDemo.vue
@@ -7,6 +7,11 @@
     <p v-else>
       {{ message }}
     </p>
+    <br><br>
+    <button @click="getHomePageListByMonth">获取本月文章</button>
+    <ul>
+      <li v-for="entry in entryList">{{ entry }}</li>
+    </ul>
   </div>
 </template>
 
@@ -16,9 +21,12 @@ export default {
   data () {
     return {
       getResult: true,
+      entryList: [],
       gridData: [],
       message: '点击网络请求获取数据',
-      apiUrl: 'http://v3.wufazhuce.com:8000/api/hp/idlist/0?'
+      apiUrl: 'http://v3.wufazhuce.com:8000/api/hp/idlist/0?',
+      apiArticleByMonthUrl: 'http://v3.wufazhuce.com:8000/api/hp/bymonth'
+      //http://v3.wufazhuce.com:8000/api/hp/bymonth/2016-09-01%2000:00:00
     }
   },
   ready: function() {
@@ -32,6 +40,23 @@ export default {
                 if (response.data.res == 0) {
                   this.getResult = true
                   this.gridData = response.data.data
+                  this.message = ''
+                } else {
+                  this.getResult = false
+                  this.message = '艾玛，啥也没找着...'
+                }
+              })
+              .catch(function(response) {
+                  console.log(response)
+              })
+      },
+      getHomePageListByMonth: function() {
+          this.$http.get(this.apiArticleByMonthUrl + "/" + new Date().toJSON().slice(0, 10))
+              .then((response) => {
+                emulateJSON: true
+                if (response.data.res == 0) {
+                  this.getResult = true
+                  this.entryList = response.data.data
                   this.message = ''
                 } else {
                   this.getResult = false
