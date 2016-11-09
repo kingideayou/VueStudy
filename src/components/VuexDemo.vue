4ca8e10d2d893f4589c4c525feefbe87baeed944
NeXT___ <kingideayou@foxmail.com>
2016-11-09 17:11:23 +0800

    vuex: state & getters

--- src/components/VuexDemo.vue
+++ src/components/VuexDemo.vue
@@ -1,5 +1,8 @@
 <template lang="html">
   <div id="app">
+    <p>{{ doneTodos }}</p>
+    <p>完成任务数：{{ doneTodosCount }}</p>
+    <br><br><br>
     <p>{{ count }}</p>
     <p>
       <button @click="increment">+</button>
@@ -12,6 +15,8 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
+// vuex 提供了独立的构建工具函数 Vuex.mapState
+import { mapState } from 'vuex'
 
 Vue.use(Vuex)
 
@@ -19,9 +24,31 @@ export default {
   data () {
     return {}
   },
+  /*
+  computed: mapState({
+    // 箭头函数可以让代码非常简洁
+    // count: state => state.count,
+    count() {
+      return store.state.count
+    }//,
+    // 传入字符串 'count' 等同于 `state => state.count`
+    // countAlias: 'count'
+    // 想访问局部状态，就必须借助于一个普通函数，函数中使用 `this` 获取局部状态
+    // countPlusLocalState (state) {
+      // return state.count + this.localCount
+    // }
+  }),
+  */
   computed: {
     count() {
       return store.state.count
+      // return this.$store.state.count
+    },
+    doneTodos() {
+      return store.getters.doneTodos
+    },
+    doneTodosCount () {
+      return store.getters.doneTodosCount
     }
   },
   mounted () {},
@@ -38,7 +65,19 @@ export default {
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    }
   },
   mutations: {
     increment: state => state.count++,
