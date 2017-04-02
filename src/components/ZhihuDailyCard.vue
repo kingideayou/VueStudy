9e474ee1c5ac661a59ee40b00ccc6d0a9cf072d0
NeXT <kingideayou@qq.com>
2017-04-02 21:57:45 +0800

    style: 知乎日报列表页样式调整

--- src/components/ZhihuDailyCard.vue
+++ src/components/ZhihuDailyCard.vue
@@ -1,17 +1,17 @@
 <template lang="html">
 
-  <div>
+  <div class="daily_card">
     <router-link :to="{ path: 'DailyDetail/'+data.id}">
       <img :src="cloudSrc" />
-      <span>
+      <div class="title">
         {{ data.title }}
-      </span><br>
-      <span>
+      </div>
+      <!-- <span>
         ga_prefix:{{ data.ga_prefix }}
       </span><br>
       <span>
         id:{{ data.id }}
-      </span>
+      </span> -->
     </router-link>
 
   </div>
@@ -41,4 +41,25 @@ export default {
 </script>
 
 <style lang="css">
+  .daily_card {
+    background: #666;
+    border-radius: 10px;
+    box-shadow: 0 0 .625rem .08rem rgba(0,0,0,.03);
+    -webkit-box-shadow: 2px 2px 6px rgba(0,0,0,0.3);
+  }
+  .title {
+    line-height: 1.5em;
+    height: 3em;       /* height is 2x line-height, so two lines will display */
+    overflow: hidden;  /* prevents extra lines from being visible */
+    padding: .5em;
+  }
+
+  /*
+  @media (-webkit-min-device-pixel-ratio: 0) {
+    .title {
+        /*display: -webkit-box;*/
+        /*-webkit-line-clamp: 2; /* number of lines to show */
+        /*-webkit-box-orient: vertical;
+    }
+}*/
 </style>
--- src/components/ZhihuDailyList.vue
+++ src/components/ZhihuDailyList.vue
@@ -1,14 +1,23 @@
 <template lang="html">
 
-  <div>
+  <div class="daily_root">
+    <div class="daily_container">
 
-    <ul>
-      <li v-for="story in stories">
-        <daily-card :data="story"></daily-card>
-      </li>
-    </ul>
+      <el-row :gutter="16">
+        <el-col class="daily_item" v-for="story in stories" :span="6">
+          <daily-card :data="story"></daily-card>
+        </el-col>
+      </el-row>
 
-    <!-- <button class="button_load_more" @click="getHomePageListByMonth(true)">查看更多</button> -->
+      <!-- <ul>
+        <li class="daily_container" v-for="story in stories">
+          <daily-card :data="story"></daily-card>
+        </li>
+      </ul> -->
+
+      <!-- <button class="button_load_more" @click="getHomePageListByMonth(true)">查看更多</button> -->
+
+    </div>
 
   </div>
 
@@ -52,4 +61,41 @@ export default {
 </script>
 
 <style lang="css">
+  .daily_root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .daily_container {
+    max-width: 860px;
+  }
+  .daily_item {
+    margin-top: 16px;
+  }
+  el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
