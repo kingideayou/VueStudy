365006632df97b6786a8f062f49087ab6bec44e3
NeXT <kingideayou@qq.com>
2017-04-13 14:14:43 +0800

    style: 调整资讯列表样式

--- src/components/Readhub.vue
+++ src/components/Readhub.vue
@@ -2,11 +2,11 @@
 
   <div class="root_content">
     <ul>
-      <li v-for="read in readList">
+      <li class="read_card" v-for="read in readList">
         <a class="card_item" target="_blank" :href="read.newsArray[0].url">
-          <div>
-            <span class="read_title">{{ read.title}}</span><br />
-            <span class="read_desc">{{ read.summary}}</span>
+          <div class="read_title">{{ read.title}}</div>
+          <div v-if="read.summary" class="read_desc">
+            {{ read.summary}}
           </div>
         </a>
       </li>
@@ -50,12 +50,31 @@ export default {
 <style lang="css">
   .read_desc {
     margin-top: 10px;
+    color: #565656;
   }
   .read_title {
-    background: #fff;
+    font-size: 1.2rem;
+    font-weight: 500;
+    line-height: 1.26em;
   }
   .card_item {
     color: #333;
     padding: 20px;
+  }
+  .root_content {
+
+  }
+  .read_card {
+    position: relative;
+    float: left;
+    width: 100%;
+    margin-right: 10px;
+    background: #ffffff;
+    border-radius: 0px;
+    margin-top: 26px;
+    margin-bottom: 26px;
+    padding-left: 16px;
+    padding-right: 16px;
+    box-shadow: 0 0 .455rem .08rem rgba(0,0,0,.05);
   }
 </style>
